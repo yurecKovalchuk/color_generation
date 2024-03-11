b33a0175b3083a91f9dd95de62cb373e12eadaec
yurecKovalchuk <KovalchukYurec@hotmail.com>
2024-03-11 11:25:24 +0200

    feat: create feature generation color, add bloc and state

--- lib/features/generation_color/screen/color_generation_screen.dart
+++ lib/features/generation_color/screen/color_generation_screen.dart
@@ -1,10 +1,30 @@
+import 'package:color_generation/features/features.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+/// Represents the screen for color generation in the application.
 class ColorGenerationScreen extends StatelessWidget {
-  const ColorGenerationScreen({super.key});
+  /// Constructor for the ColorGenerationScreen.
+  const ColorGenerationScreen({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final bloc = BlocProvider.of<ColorGenerationBloc>(context);
+
+    return BlocBuilder<ColorGenerationBloc, ColorGenerationState>(
+      builder: (context, state) {
+        return GestureDetector(
+          onTap: bloc.setRandomColor,
+          child: Scaffold(
+            backgroundColor: state.backgroundColor,
+            body: const Center(
+              child: Text('Hello there'),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
